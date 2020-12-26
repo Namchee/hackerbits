24f80579e2d886ab982bc4dd548b5d0e184099d5
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-12-26 21:06:05 +0700

    chore: Add ML and plotting library dependencies

---
+++ src/clustering.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from src.model.news import News
+
+def do_flat_clustering(news: List[News]):
+
+
+def get_clusters_with_elbow():
+    
+
+def tf_idf(news: List[News]):
+    
