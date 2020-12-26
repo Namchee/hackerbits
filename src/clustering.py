979b9921fc27eee50aea501e32728750b7870bd0
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-12-26 21:18:03 +0700

    chore: Define structure for clustering file

--- src/clustering.py
+++ src/clustering.py
@@ -1,12 +1,35 @@
+from nltk import tokenize, word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from re import search
 from typing import List
 
 from src.model.news import News
 
-def do_flat_clustering(news: List[News]):
-
+def k_means(news: List[News]):
+    return None
 
 def get_clusters_with_elbow():
-    
+    return None
 
-def tf_idf(news: List[News]):
+def _tokenize(text: str) -> List[str]:
+    """Generate tokens from a long text
+
+    Args:
+        text (str): Text to be processed.
+
+    Returns:
+        List[str]: List of tokens
+    """
+    tokens = word_tokenize(text)
+    tokens = [token.lower() for token in tokens]
+
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(token) for token in tokens]
+
+    tokens = list(filter(lambda token: token not in stopwords.words('english', tokens)))
     
+    tokens = list(filter(lambda token: search("[^a-zA-Z-]+", token) is None, tokens))
+
+    return tokens
