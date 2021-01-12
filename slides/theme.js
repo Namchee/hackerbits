7f3524a44ccc8f99b18680b5179986d65dba816e
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-01-12 21:35:33 +0700

    feat: Add MDX deck skeleton

---
+++ slides/theme.js
@@ -0,0 +1,6 @@
+export default {
+  fonts: {
+    body: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    monospace: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace',
+  },
+}
