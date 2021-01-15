aa8dc8888e54b6485e22b74e78f68bc88d575f9c
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-01-15 07:17:27 +0700

    chore: Remove slides as mdx-deck is overkill

--- slides/theme.js
+++
@@ -1,6 +0,0 @@
-export default {
-  fonts: {
-    body: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-    monospace: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace',
-  },
-}
